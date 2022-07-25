8eacac379e2ea7808916563d4d70305df7c22404
tohbey <fafoworatobi25@gmail.com>
2022-07-25 16:55:58 +0100

    chef-service setup

---
+++ cook-unity-chef-service/eureka.js
@@ -0,0 +1,33 @@
+const Eureka  = require("eureka-js-client").Eureka;
+
+module.exports = function(PORT){
+    const client = new Eureka({
+        instance:{
+            app:"cook-unit-chef-service",
+            hostName: 'localhost',
+            ipAddr:'127.0.0.1',
+            statusPageUrl:'http://localhost:3000',
+            vipAddress:'cook-unit-chef-service',
+            port :{
+                $:PORT,
+                '@enabled': 'true'
+            },
+            dataCenterInfo:{
+                '@class':'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+                name: 'MyOwn'
+            },
+            registerWithEureka: true,
+            fetchReistry: true
+        },
+        eureka: {
+            host:'localhost',
+            port:8010,
+            servicePath:'/eureka/apps/'
+        },
+    });
+    
+    client.logger.level('debug')
+    client.start(error => {
+        console.log(error || "Nodejs Eureka Started");
+    })
+}
