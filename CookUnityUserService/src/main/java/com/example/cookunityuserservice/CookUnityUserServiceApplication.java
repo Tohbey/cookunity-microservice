052135ef7259a68ff399d9bf688beda9e9cd145c
tohbey <fafoworatobi25@gmail.com>
2022-07-25 10:20:27 +0100

    User service setup

---
+++ CookUnityUserService/src/main/java/com/example/cookunityuserservice/CookUnityUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.cookunityuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CookUnityUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookUnityUserServiceApplication.class, args);
+    }
+
+}
