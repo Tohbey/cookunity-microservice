e26d1cc4d8cd0ca1acbb92957fc0f6516eee2db1
tobey <fafoworatobi25@gmail.com>
2022-07-27 14:40:28 +0100

    Transaction Service setup

---
+++ CookUnityTransactionService/src/main/java/com/example/cookunitytransactionservice/CookUnityTransactionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.cookunitytransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CookUnityTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookUnityTransactionServiceApplication.class, args);
+    }
+
+}
