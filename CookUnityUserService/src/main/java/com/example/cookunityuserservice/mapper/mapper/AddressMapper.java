c11dc6fdb62e57e2cdb657b7ea6f97e2e3bdfa22
tobey <fafoworatobi25@gmail.com>
2022-08-02 13:09:55 +0100

    mapper interfaces setup for mappers: User service final

--- CookUnityUserService/src/main/java/com/example/cookunityuserservice/mapper/mapper/AddressMapper.java
+++ CookUnityUserService/src/main/java/com/example/cookunityuserservice/mapper/mapper/AddressMapper.java
@@ -1,9 +1,7 @@
 package com.example.cookunityuserservice.mapper.mapper;
 
 import com.example.cookunityuserservice.mapper.DTO.AddressDTO;
-import com.example.cookunityuserservice.mapper.DTO.UserDTO;
 import com.example.cookunityuserservice.model.Address;
-import com.example.cookunityuserservice.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
