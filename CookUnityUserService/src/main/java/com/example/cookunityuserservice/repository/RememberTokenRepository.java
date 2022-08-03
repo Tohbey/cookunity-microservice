eb38c4f6bbf6e3539a8f09156cd4f500393eef2e
tobey <fafoworatobi25@gmail.com>
2022-08-03 20:11:41 +0100

    Addition of function in interface.

--- CookUnityUserService/src/main/java/com/example/cookunityuserservice/repository/RememberTokenRepository.java
+++ CookUnityUserService/src/main/java/com/example/cookunityuserservice/repository/RememberTokenRepository.java
@@ -1,9 +1,12 @@
 package com.example.cookunityuserservice.repository;
 
+import com.example.cookunityuserservice.model.RememberToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
-public interface RememberTokenRepository extends JpaRepository<RememberTokenRepository, UUID> {
+public interface RememberTokenRepository extends JpaRepository<RememberToken, UUID> {
+    Optional<RememberToken> findRememberTokenByToken(String token);
 }
--- CookUnityUserService/src/main/java/com/example/cookunityuserservice/repository/SecretQuestionRepository.java
+++ CookUnityUserService/src/main/java/com/example/cookunityuserservice/repository/SecretQuestionRepository.java
@@ -1,10 +1,13 @@
 package com.example.cookunityuserservice.repository;
 
+import com.example.cookunityuserservice.model.SecretQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface SecretQuestionRepository extends JpaRepository<SecretQuestionRepository, UUID> {
+public interface SecretQuestionRepository extends JpaRepository<SecretQuestion, UUID> {
+    Optional<SecretQuestion> findSecretQuestionByQuestion(String question);
 }
