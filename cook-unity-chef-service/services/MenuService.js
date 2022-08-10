19d478bfd7a17ca1a2dc190ebe827f7393ced860
tobey <fafoworatobi25@gmail.com>
2022-08-10 14:09:26 +0100

    UPDATE: implementation of menu service

--- cook-unity-chef-service/services/MenuService.js
+++ cook-unity-chef-service/services/MenuService.js
@@ -3,50 +3,119 @@ const { MSG_TYPES } = require('../constant/types');
 
 class MenuService{
 
-    static create(){
+    /**
+     * Create Menu
+     * @param {Object} body Menu
+     */
+    static create(body){
         return new Promise(async (resolve, reject) => {
             try {
+                const menu = await Menu.findOne({
+                    chef: body.chef,
+                    title: body.title
+                });
 
+                if(menu){
+                    reject({ statusCode: 404, msg: MSG_TYPES.EXIST });
+                    return;
+                }
+
+                const newMenu = new Menu(body)
+                await newMenu.save();
+
+                resolve(newMenu);
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
         })
     }
 
-    static getMenus(){
+    /**
+     * Get Menus
+     * @param {Number} skip skip
+     * @param pagesSize
+     * @param {Object} filter filter
+     */
+    static getMenus(skip, pageSize, filter={}){
         return new Promise(async (resolve, reject) => {
             try {
+                const menus = await Menu.find(filter)
+                    .skip(skip).limit(pageSize);
 
+                const total = await Menu.find(filter).countDocuments();
+
+                resolve({menus, total});
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
         })
     }
 
-    static getMenu(){
+    /**
+     * Get chef
+     * @param {Object} filter filter
+     */
+    static getMenu(filter){
         return new Promise(async (resolve, reject) => {
             try {
+                const menu = await Menu.findOne(filter);
+                if(!menu){
+                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND });
+                }
 
+                resolve(menu);
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
         })
     }
 
-    static updateMenu(){
+    /**
+     * Update Menu
+     * @param {object} menuId menu's id
+     * @param {Object} menuObject updated details
+     */
+    static updateMenu(menuId, menuObject){
         return new Promise(async (resolve, reject) => {
             try {
+                const menu = await Menu.findById(menuId);
 
+                if (!menu) {
+                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND });
+                }
+
+                await menu.updateOne(
+                    {
+                        $set: menuObject
+                    }
+                );
+                resolve(menu);
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
         })
     }
 
-    static deleteMenu(){
+
+    /**
+     * Delete Chef
+     * @param {Object} chef Chef
+     * @param {Object} menuId menuId
+     * */
+    static deleteMenu(chef, menuId){
         return new Promise(async (resolve, reject) => {
             try {
+                const menu = await Menu.findOne({
+                    chef: chef,
+                    _id: menuId
+                });
+                if (!menu) {
+                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND });
+                }
 
+                await menu.delete();
+
+                resolve({ msg: MSG_TYPES.DELETED });
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
