0798dd20e991887a7eeb1ab38757edfc06e17a2e
tobey <fafoworatobi25@gmail.com>
2022-08-10 10:43:28 +0100

    UPDATE: chef service fix

--- cook-unity-chef-service/services/ChefService.js
+++ cook-unity-chef-service/services/ChefService.js
@@ -2,10 +2,30 @@ const Chef = require('../models/Chef');
 const { MSG_TYPES } = require('../constant/types');
 
 class ChefService{
-    static create(){
+
+    /**
+     * Create Chef
+     * @param {Object} body Chef
+     */
+    static create(body){
         return new Promise(async (resolve, reject) => {
             try {
+                body.emailChef = body.emailChef+"CHEF";
 
+                const chef = await Chef.findOne({
+                    userId: body.userId,
+                    emailChef: body.emailChef
+                });
+
+                if(chef){
+                    reject({ statusCode: 404, msg: MSG_TYPES.EXIST });
+                    return;
+                }
+
+                const newChef = new Chef(body);
+                await newChef.save();
+
+                resolve(newChef);
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
@@ -22,11 +42,11 @@ class ChefService{
         return new Promise(async (resolve, reject) => {
             try {
                 const chefs = await Chef.find(filter)
-                    .skip(skip).limit(pagesSize)
+                    .skip(skip).limit(pagesSize);
 
-                const total = await Chef.find(filter).countDocuments()
+                const total = await Chef.find(filter).countDocuments();
 
-                resolve({chefs, total})
+                resolve({chefs, total});
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
@@ -40,12 +60,12 @@ class ChefService{
     static getChef(filter){
         return new Promise(async (resolve, reject) => {
             try {
-                const chef = await Chef.findOne(filter)
+                const chef = await Chef.findOne(filter);
                 if(!chef){
-                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND })
+                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND });
                 }
 
-                resolve(chef)
+                resolve(chef);
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
@@ -62,15 +82,15 @@ class ChefService{
                 const currentChef = await Chef.findOne({
                     _id: chef._id,
                     status: "Active"
-                })
+                });
                 if (!currentChef) {
-                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND })
+                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND });
                 }
 
                 currentChef.status = "Terminated";
                 await currentChef.save();
 
-                resolve({ msg: MSG_TYPES.DELETED })
+                resolve({ msg: MSG_TYPES.DELETED });
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
@@ -88,15 +108,15 @@ class ChefService{
                 const chef = await Chef.findById(chefId);
 
                 if (!chef) {
-                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND })
+                    return reject({ statusCode: 404, msg: MSG_TYPES.NOT_FOUND });
                 }
 
                 await chef.updateOne(
                     {
                         $set: chefObject
                     }
-                )
-                resolve(chef)
+                );
+                resolve(chef);
             }catch (error) {
                 reject({statusCode:500, msg: MSG_TYPES.SERVER_ERROR, error})
             }
