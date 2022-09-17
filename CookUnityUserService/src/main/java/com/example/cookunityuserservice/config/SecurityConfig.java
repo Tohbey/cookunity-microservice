4542d5c11240a5314e491a9b784dc919ca500288
tobey <fafoworatobi25@gmail.com>
2022-09-17 22:21:27 +0100

    UPDATE: security config fix

--- CookUnityUserService/src/main/java/com/example/cookunityuserservice/config/SecurityConfig.java
+++ CookUnityUserService/src/main/java/com/example/cookunityuserservice/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.cookunityuserservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,7 +21,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    @Bean
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
